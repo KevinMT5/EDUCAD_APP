3c389e2d8acf444482c6558149f9d425efafb890
KevinMT5 <kevinalexis809@gmail.com>
2025-09-22 19:21:25 -0600

    Configurar el primer archivo

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('EDUCAD_APP');
+
+  titulo='EDUCADAPP';
+
 }
