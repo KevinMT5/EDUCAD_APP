cb7b08ccff43602fa249a2a384db07596d57a847
KevinMT5 <kevinalexis809@gmail.com>
2025-09-24 18:49:31 -0600

    Agregando forms de prueba

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Empleado } from './empleado.model';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -12,4 +13,11 @@ export class App {
 
   titulo='EDUCADAPP';
 
+  empleado:Empleado = new Empleado('','','',0);
+
+
+  registrarEmpleado():void{
+    console.log(`Usuario registrado: ${this.empleado.nombre} ${this.empleado.apellido}, Cargo: ${this.empleado.cargo}, Salario: ${this.empleado.salario}`);
+    this.empleado = new Empleado('','','',0);
+  }
 }
