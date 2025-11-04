fa1d0eebcaa339a694d5c1cd7607e1c4fc490888
KevinMT5 <kevinalexis809@gmail.com>
2025-11-03 18:38:10 -0600

    Actualizacion

--- src/app/cursos-component/cursos.model.ts
+++ src/app/cursos-component/cursos.model.ts
@@ -1,14 +1,17 @@
-// src/app/cursos-component/cursos.model.ts
 export class Course {
   nombre: string = "";
   descripcion: string = "";
   duracion: string = "";
   precio: number = 0;
+  imagen?: string;
+  categoria?: string; // ✅ Nuevo campo opcional
 
-  constructor(nombre: string, descripcion: string, duracion: string, precio: number) {
+  constructor(nombre: string, descripcion: string, duracion: string, precio: number, categoria?: string, imagen?: string) {
     this.nombre = nombre;
     this.descripcion = descripcion;
     this.duracion = duracion;
     this.precio = precio;
+    this.categoria = categoria || ''; // ✅ inicialización opcional
+    this.imagen = imagen || '';       // ✅ inicialización opcional
   }
 }
